41b08dc86c11f155caf9f88219ab62bb7d8c15a1
Mark Henderson <mark@mrh.io>
2022-09-11 12:51:42 -0400

    fix: io shim

--- src/utils/index.js
+++ src/utils/index.js
@@ -3,11 +3,13 @@
 import difference from './difference.js'
 import findUniques from './find-uniques.js'
 import isDefined from './is-defined.js'
-import io from 'orbit-db-io'
+import { read, write } from 'orbit-db-io'
+
+const io = { read, write }
 
 export {
   difference,
   findUniques,
   isDefined,
-  io  
+  io 
 }
