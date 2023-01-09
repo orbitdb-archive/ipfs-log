70a66db6b5c71271a7f6e9db2032506e3ad0dc5a
Hayden Young <hayden.y@minertoken.io>
2023-01-09 22:33:10 +0000

    fix: webpack builds.

--- conf/webpack.tests.config.js
+++ conf/webpack.tests.config.js
@@ -1,16 +1,9 @@
 import glob from 'glob'
 import webpack from 'webpack'
-import path from 'path'
 import { createRequire } from 'module'
-import { fileURLToPath } from 'url'
-
-import NodePolyfillPlugin from 'node-polyfill-webpack-plugin'
 
 export default (env, argv) => {
   const require = createRequire(import.meta.url)
-  const __filename = fileURLToPath(import.meta.url)
-  const __dirname = path.dirname(__filename)
-
   return {
     // TODO: put all tests in a .js file that webpack can use as entry point
     entry: glob.sync('./test/*.spec.js', { ignore: ['./test/replicate.spec.js'] }),
@@ -18,62 +11,44 @@ export default (env, argv) => {
       filename: '../test/browser/bundle.js'
     },
     target: 'web',
+    mode: 'production',
     devtool: 'source-map',
-    mode: 'development',
     plugins: [
-      new webpack.DefinePlugin({
-        'process.env': {
-          NODE_ENV: JSON.stringify(process.env.NODE_ENV)
-        }
-      }),
-      new NodePolyfillPlugin()
+      new webpack.ProvidePlugin({
+        Buffer: ['buffer', 'Buffer']
+      })
     ],
-    externals: {
-      fs: '{ existsSync: () => true }',
-      fatfs: '{}',
-      'fs-extra': '{ copy: () => {} }',
-      rimraf: '{ sync: () => {} }',
-      'idb-readable-stream': '{}',
-      runtimejs: '{}',
-      net: '{}',
-      child_process: {},
-      dns: '{}',
-      tls: '{}',
-      bindings: '{}'
+    experiments: {
+      topLevelAwait: true
     },
     resolve: {
       modules: [
-        'node_modules',
-        path.resolve(__dirname, '../node_modules')
+        'node_modules'
       ],
       fallback: {
-        assert: require.resolve('assert'),
         path: require.resolve('path-browserify'),
-        stream: require.resolve('stream-browserify')
+        os: false,
+        fs: false,
+        constants: false,
+        stream: false
       }
+    },
+    externals: {
+      'fs-extra': '{ copy: () => {} }',
+      rimraf: '{ sync: () => {} }'
     },
     module: {
       rules: [
         {
-          test: /\.js$/,
+          test: /\.m?js$/,
           exclude: /node_modules/,
           use: {
             loader: 'babel-loader',
             options: {
-              plugins: [
-                '@babel/syntax-object-rest-spread',
-                '@babel/transform-runtime',
-                '@babel/plugin-transform-modules-commonjs',
-                '@babel/plugin-proposal-nullish-coalescing-operator',
-                '@babel/plugin-proposal-optional-chaining'
-              ]
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-syntax-import-assertions']
             }
           }
-        },
-        {
-          // For inlining the fixture keys in browsers tests
-          test: /userA|userB|userC|userD|0358df8eb5def772917748fdf8a8b146581ad2041eae48d66cc6865f11783499a6|032f7b6ef0432b572b45fcaf27e7f6757cd4123ff5c5266365bec82129b8c5f214|02a38336e3a47f545a172c9f77674525471ebeda7d6c86140e7a778f67ded92260|03e0480538c2a39951d054e17ff31fde487cb1031d0044a037b53ad2e028a3e77c$/,
-          loader: 'json-loader'
         }
       ]
     }
